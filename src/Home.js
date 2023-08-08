7b7bd31a5bff50b66970c4e842b18d83e934bec6
Zukhra <zuhra.galieva.2014@gmail.com>
2023-08-08 19:49:06 +0100

    add and handle click event

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,11 @@
 const Home = () => {
+    const handleClick = () => {
+        console.log('Hello!');
+    }
     return (
         <div className="home">
             <h2>Home page</h2>
+            <button onClick={handleClick}>Click me</button>
         </div>
     );
 }
