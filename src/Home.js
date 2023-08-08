9836e73c64a0bb736773d07d1648b4913e79c1f8
Zukhra <zuhra.galieva.2014@gmail.com>
2023-08-08 20:53:42 +0100

    Add the state(reactive) values

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,16 @@
+import { useState } from "react";
+
 const Home = () => {
+    const [name, setName] = useState('Mario')
+    const [age, setAge] = useState(18)
     const handleClick = () => {
-        console.log('Hello!');
+        setName('Leo');
+        setAge(9)
     }
     return (
         <div className="home">
             <h2>Home page</h2>
+            <p>{name} is {age} years old!</p>
             <button onClick={handleClick}>Click me</button>
         </div>
     );
