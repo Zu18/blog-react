12a27e23f017d3469681bd42f58e0fb25dc6d8ff
Zukhra <zuhra.galieva.2014@gmail.com>
2023-08-09 09:03:01 +0100

    Modify the Home page, render blogs and authors

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,21 @@
 import { useState } from "react";
 
 const Home = () => {
-    const [name, setName] = useState('Mario')
-    const [age, setAge] = useState(18)
-    const handleClick = () => {
-        setName('Leo');
-        setAge(9)
-    }
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ])
     return (
         <div className="home">
-            <h2>Home page</h2>
-            <p>{name} is {age} years old!</p>
-            <button onClick={handleClick}>Click me</button>
+            {blogs.map(blog => (
+                <div className="blog-preview" key={blog.id} >
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                </div>
+            ))}
         </div>
     );
 }
- 
+
 export default Home;
