779b023e397db752ea20d3978581c9ab0ab92935
Zukhra <zuhra.galieva.2014@gmail.com>
2023-08-10 11:04:33 +0100

    Impliment useEffect with dependency

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -7,13 +7,23 @@ const Home = () => {
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ])
+    const [name, setName] = useState('Mario')
+
     const handleDelete = (id) => {
-        const newBlog = blogs.filter(blog =>blog.id !== id);
+        const newBlog = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlog);
     }
+
+    useEffect(() => {
+        console.log('use effect ran');
+        console.log(name);
+    }, [name])
+    
     return (
         <div className="home">
             <BlogList blogs={blogs} title='All Blogs:' handleDelete={handleDelete} />
+            <button onClick={()=> setName('Leo')}>Change name</button>
+            <p>{name}</p>
         </div>
     );
 }
